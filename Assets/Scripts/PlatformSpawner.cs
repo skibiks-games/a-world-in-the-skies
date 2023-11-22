652463bdf327df0abe4a3731e267131fe3040d8d
Chathet <w.bochenski.05@gmail.com>
2023-11-23 00:20:45 +0100

    es

---
+++ Assets/Scripts/PlatformSpawner.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformSpawner : MonoBehaviour
+{
+    [SerializeField] float spawnRate;
+
+    void Update()
+    {
+        float timer = 0;
+        if(timer >= spawnRate)
+        {
+            
+        }
+    }
+}
