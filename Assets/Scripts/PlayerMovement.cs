98ac2df595e63517c7692dfd8bebc5b21d573a25
skibiks-games <skibiks.games@gmail.com>
2023-11-23 00:10:25 +0100

    Added player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] private float sideSpeed;
+    [SerializeField] private float jumpForce;
+
+    private Rigidbody2D _rb;
+
+    private void Start() {
+        _rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Jump();
+        }
+
+        float sideMovement = Input.GetAxis("Horizontal") * sideSpeed * Time.deltaTime;
+        transform.position += Vector3.right * sideMovement;
+    }
+
+    private void Jump() {
+        _rb.velocity = Vector2.zero;
+        _rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
     }
 }
