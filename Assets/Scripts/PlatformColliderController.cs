3414673a13311234db9f074469065e448b755241
skibiks-games <skibiks.games@gmail.com>
2023-11-23 01:47:06 +0100

    Fixed platform scripts

---
+++ Assets/Scripts/PlatformColliderController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlatformColliderController : MonoBehaviour
+{
+    private static PlayerMovement PlayerMovement => PlayerMovement.Instance;
+
+    private BoxCollider2D _collider;
+    private Transform _playerTF;
+
+    private float _minDistanceToJump;
+
+    private void Start() {
+        _collider = GetComponent<BoxCollider2D>();
+        _playerTF = PlayerMovement.transform;
+
+        _minDistanceToJump = (transform.localScale.y + _playerTF.localScale.y) * .49f;
+    }
+
+    private void Update() {
+        _collider.enabled = _playerTF.position.y > transform.position.y + _minDistanceToJump;
+    }
+}
