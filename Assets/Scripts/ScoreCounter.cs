c2e1334ce56c7f4b7705d88d7baf4bd97ce5dffa
skibiks-games <skibiks.games@gmail.com>
2023-11-23 02:10:13 +0100

    Added PP for max score

--- Assets/Scripts/ScoreCounter.cs
+++ Assets/Scripts/ScoreCounter.cs
@@ -3,11 +3,25 @@ using UnityEngine;
 public class ScoreCounter : MonoBehaviour
 {
     public int score;
+    public int maxScore;
+
+    private static string maxScorePPId = "MaxScore";
 
     [SerializeField] private LayerMask coinsLayer;
 
     private void Start() {
         score = 0;
+
+        if (PlayerPrefs.HasKey(maxScorePPId))
+            maxScore = PlayerPrefs.GetInt(maxScorePPId);
+        else {
+            PlayerPrefs.SetInt(maxScorePPId, 0);
+            maxScore = 0;
+        }
+    }
+
+    private void UpdateUI() {
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision) {
